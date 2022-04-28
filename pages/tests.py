6d7546171bbc11561a3a20ef1760507753292570
Kevin <armyveteranoif247@gmail.com>
2022-04-27 20:05:32 -0500

    Added test case

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,36 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+class SimpleTestCase(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_status_code(self):
+        response = self.client.get('/about/')
+        self.assertEqual(response.status_code, 200) 
+
+    def test_home_page_contains_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, "Hello, World!")
+
+    def test_home_page_uses_correct_templates(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_about_page_contains_content(self):
+        response = self.client.get('/about/')
+        self.assertContains(response, 'My Name is Kevin.')
+
+    def test_home_page_reverse_lookup(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_about_page_reverse_lookup(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
+
